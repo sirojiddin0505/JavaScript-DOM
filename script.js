c9507607190951359913477a450f0c01873267a7
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-03-04 12:34:14 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// let text = document.getElementById("text");
+// text.textContent = "salom"
+// console.log(text.textContent);
+// let text = document.getElementsByClassName("texts");
+// text.textContent = "salom"
+// console.log(text.textContent);
+
+// let text = document.getElementById("text");
+// let submit = document.getElementById("btn");
+// btn.addEventListener("click", function(){
+//     text.classList.add("text-color")
+// })
+
+// UYGA VAZIFA
+// 1
+// let text = document.getElementById("text");
+// let btn = document.getElementById("btn");
+// btn.addEventListener('click', function(){
+//     text.classList.add("color")
+// })
+// text.addEventListener("click", function(){
+//     btn.classList.add("btn")
+// })
+
+
+// 2
