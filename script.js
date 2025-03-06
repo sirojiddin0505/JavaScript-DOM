c3fac9c3048dfc9d56da91fc64651e0706c713d1
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-03-06 22:08:49 +0500

    first  commit

--- script.js
+++ script.js
@@ -11,16 +11,41 @@
 //     text.classList.add("text-color")
 // })
 
-// UYGA VAZIFA
-// 1
-let text = document.getElementById("text");
-let btn = document.getElementById("btn");
-btn.addEventListener('click', function(){
-    text.classList.add("color")
-})
-text.addEventListener("click", function(){
-    btn.classList.add("btn")
-})
+// let text = document.getElementById("text");
+// let btn1 = document.getElementById("btn1");
+// let btn2 = document.getElementById("btn2");
+// btn1.addEventListener("click", function(){
+//     text.style.color = "blue"
+// })
+// btn2.addEventListener("click", function(){
+//     text.style.color = 'red'
+
+// })
+
+// yangi element qo'shish👇
+
+// let navList = document.getElementById('nav__list')
+// let navItem = document.createElement('li')
+// navItem.textContent = 'salom'
+// navList.appendChild(navItem)
 
 
-// 2
+// funksiya yaratish👇
+
+// function hour(){
+//     alert("soat 12 bo'ldi")
+// }
+// let btn = document.getElementById('btn')
+// btn.style.padding = "15px"
+// btn.style.fontSize = "25px"
+
+
+// soat yaratish
+// let hour = document.getElementById('hour')
+
+// function getHours(){
+//     let date = new Date()
+//     hour.textContent = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds().toString().padStart(2, "0")}`
+//     setInterval(()=>getHours(), 100)
+// }
+// getHours()
